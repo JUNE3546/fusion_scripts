5ae272b7db1fd51051dc3ff341e0eb2d3d6f0c5c
Alex Bogomolov <mail@abogomolov.com>
2019-06-08 09:26:04 +0300

    fix slow copy/paste tools

--- Scripts/Comp/my_tools/duplicateTool.lua
+++ Scripts/Comp/my_tools/duplicateTool.lua
@@ -1,25 +1,22 @@
--- 22.02.2019 fix compatibility with Fusion 9
--- 15.05.2019 fix slow copy/paste issue
+-- v1.0 Initial script created by Sven Neve
+-- v1.1 22.02.2019 fix compatibility with Fusion 9+
+-- v1.2 15.05.2019 fix slow copy/paste Loaders issue, simplify error checking
 
 local originalToolList = comp:GetToolList(true)
 flow = comp.CurrentFrame.FlowView
 composition:StartUndo("Duplicate")
 comp:Copy(originalToolList)
--- comp:SetActiveTool()
+comp:SetActiveTool()
 flow:Select()
 comp:Paste()
 local duplicateToolList = comp:GetToolList(true)
 
-for i, tool in pairs(originalToolList) do
-	for j, input in pairs(tool:GetInputList()) do
+for tool_index, tool in pairs(originalToolList) do
+	for input_index, input in pairs(tool:GetInputList()) do
 		if input:GetAttrs().INPB_Connected then
-			-- if duplicateToolList[i]:GetInputList()[j] ~= nil then
-            if not duplicateToolList[i]:GetInputList()[j]:GetAttrs().INPB_Connected then
-                duplicateToolList[i]:GetInputList()[j]:ConnectTo(input:GetConnectedOutput())
+            if not duplicateToolList[tool_index]:GetInputList()[input_index]:GetAttrs().INPB_Connected then
+                duplicateToolList[tool_index]:GetInputList()[input_index]:ConnectTo(input:GetConnectedOutput())
             end
-			-- else
-				-- print("----ERROR : Input mismatch, nil value found")
-			-- end			
 		end
 	end
 end
